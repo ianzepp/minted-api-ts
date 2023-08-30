09231d6bbde50716f1b2d1a88a19d990f7f4cfad
Ian Zepp <ian.zepp@protonmail.com>
2023-08-30 14:53:50 -0400

    Implemented code to preload record data from DB

---
+++ src/observers/record.load-prev.ts
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+
+// Classes
+import { Observer } from '../classes/observer';
+import { ObserverFlow } from '../classes/observer-flow';
+import { ObserverRing } from '../classes/observer';
+import { SchemaType } from '../classes/schema';
+import { RecordFlat } from '../classes/record';
+
+export default class extends Observer {
+    toName() {
+        return 'record.load-prev';
+    }
+    
+    onSchema() {
+        return SchemaType.Record;
+    }
+
+    onRing() {
+        return ObserverRing.Init;
+    }
+
+    onUpdate() {
+        return true;
+    }
+
+    async run(flow: ObserverFlow) {
+        let result: RecordFlat[] = await flow.statement.whereIn('id', flow.change_ids).select();
+        let result_map = _.keyBy(result, 'id');
+
+        // Assign the raw knex data for previous values to the records
+        for(let record of flow.change) {
+            let record_prev = result_map[record.data.id];
+
+            if (record_prev === undefined) {
+                continue; // TODO Is this an error? We have an ID but no record found?
+            }
+
+            record.fromRecordPrev(record_prev);
+        }
+    }
+}
