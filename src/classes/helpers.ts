3125ce3d779b46da7c26ab2b6b780a341de7f263
Aiden Zepp <aiden.zepp@protonmail.com>
2023-09-05 20:04:19 -0400

    Added fn return type for `migrationInsertAll`.

--- src/classes/helpers.ts
+++ src/classes/helpers.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { Knex } from 'knex';
 
-export async function migrationInsertAll(knex: Knex, table_name: string, record_data: _.Dictionary<any>[]) {
+export async function migrationInsertAll(knex: Knex, table_name: string, record_data: _.Dictionary<any>[]): Promise<_.Dictionary<any>> {
     for(let record of record_data) {
         console.warn('migrationInsert', record);
 
