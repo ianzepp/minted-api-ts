046bb8cf2de9a767dc2adf210114689c08f7405d
Aiden Zepp <aiden.zepp@protonmail.com>
2023-09-05 20:12:26 -0400

    Added fn return type for `tableDown`.

--- src/helpers/tableDown.ts
+++ src/helpers/tableDown.ts
@@ -1,6 +1,6 @@
 import { Knex } from 'knex';
 
-export default async function(knex: Knex, table_name: string) {
+export default async function(knex: Knex, table_name: string): Promise<void> {
     await knex.schema.dropTable(table_name + '_acls');
     await knex.schema.dropTable(table_name + '_info');
     await knex.schema.dropTable(table_name);
