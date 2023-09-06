ef092dc2cdba1aca9e81dd4a6c06dab257ba8f3a
Aiden Zepp <aiden.zepp@protonmail.com>
2023-09-05 20:05:03 -0400

    Dunder + fn return types for `SystemMeta`.

--- src/classes/system-meta.ts
+++ src/classes/system-meta.ts
@@ -13,14 +13,14 @@ import { System } from '../classes/system';
 
 export class SystemMeta {
     // Cache known schema and column names
-    private readonly _schema_dict: _.Dictionary<Schema> = {};
-    private readonly _column_dict: _.Dictionary<Column> = {};
+    private readonly __schema_dict: _.Dictionary<Schema> = {};
+    private readonly __column_dict: _.Dictionary<Column> = {};
 
-    constructor(private readonly system: System) {}
+    constructor(private readonly __system: System) {}
 
-    async startup() {
+    async startup(): Promise<void> {
         let select_data = async (schema_name: string) => {
-            return this.system.knex.select(schema_name, ['*']).then(results => {
+            return this.__system.knex.select(schema_name, ['*']).then(results => {
                 return _.map(results, source => {
                     return new Record(schema_name).fromRecordFlat(source);
                 });
@@ -32,13 +32,13 @@ export class SystemMeta {
         let columns = _.map(await select_data('column'), source => new Column(source));
 
         // Add to the known dict data
-        _.each(schemas, schema => this._schema_dict[schema.toFullName()] = schema);
-        _.each(columns, column => this._column_dict[column.toFullName()] = column);
+        _.each(schemas, schema => this.__schema_dict[schema.toFullName()] = schema);
+        _.each(columns, column => this.__column_dict[column.toFullName()] = column);
 
         // Post-process columns
-        for(let column_name in this._column_dict) {
-            let column = this._column_dict[column_name];
-            let schema = this._schema_dict[column.schema_name];
+        for(let column_name in this.__column_dict) {
+            let column = this.__column_dict[column_name];
+            let schema = this.__schema_dict[column.schema_name];
 
             // Orphaned column?
             if (schema === undefined) {
@@ -53,15 +53,15 @@ export class SystemMeta {
 
 
     isSchema(schema_name: string): boolean {
-        return this._schema_dict[schema_name] instanceof Schema;
+        return this.__schema_dict[schema_name] instanceof Schema;
     }
 
     isColumn(column_name: string): boolean {
-        return this._column_dict[column_name] instanceof Column;
+        return this.__column_dict[column_name] instanceof Column;
     }
 
     toSchema(schema_name: string): Schema {
-        let schema = this._schema_dict[schema_name];
+        let schema = this.__schema_dict[schema_name];
 
         if (schema === undefined) {
             throw `Schema '${schema_name}' not found/loaded`;
@@ -70,38 +70,38 @@ export class SystemMeta {
         return schema;
     }
 
-    toColumn(schema_name: string, column_name: string): Column | undefined{
-        return this._column_dict[schema_name + '.' + column_name];
+    toColumn(schema_name: string, column_name: string): Column | undefined {
+        return this.__column_dict[schema_name + '.' + column_name];
     }
 
-    toFilter(schema_name: string, filter_data: _.Dictionary<any>) {
+    toFilter(schema_name: string, filter_data: _.Dictionary<any>): Filter {
         let record_data = _.merge(filter_data, { schema_name: schema_name });
         let record = new Record('filter').fromRecordFlat(record_data);
         return new Filter(record);
     }
 
-    toChange(schema_name: string, change_data: Partial<RecordJson>[]) {
+    toChange(schema_name: string, change_data: Partial<RecordJson>[]): Record[] {
         return change_data.map(change => new Record(schema_name).fromRecordJson(change));
     }
 
     describe(): Object {
         let result = {
-            schemas: this._schema_dict,
-            columns: this._column_dict
+            schemas: this.__schema_dict,
+            columns: this.__column_dict
         };
 
         // Type convert
         return JSON.parse(JSON.stringify(result));
     }
 
-    async import(source: unknown) {
-        
+    async import(source: unknown): Promise<void> {
+        // ...
     }
 
-    async export() {
+    async export(): Promise<Object> {
         return {
-            schemas: _.map(this._schema_dict, schema => schema.toJSON()),
-            columns: _.map(this._column_dict, column => column.toJSON()),
+            schemas: _.map(this.__schema_dict, schema => schema.toJSON()),
+            columns: _.map(this.__column_dict, column => column.toJSON()),
         };
     }
 }
