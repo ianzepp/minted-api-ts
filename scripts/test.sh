17f4ae9c25f971adb1c830f37a264ae310c8925c
Ian Zepp <ian.zepp@protonmail.com>
2023-09-07 12:59:35 -0400

    Moved env to job-level

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,13 +7,10 @@ echo "Using POSTGRES_DB=$POSTGRES_DB"
 echo "Using POSTGRES_USER=$POSTGRES_USER"
 
 # Compile everything
-if ! npm run compile:clean; then
+if ! npm run compile; then
     echo "Compilation failed!"
     exit 1
 fi
-
-# Run the migration
-knex migrate:latest
 
 # Start node
 jest
