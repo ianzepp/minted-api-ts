0d86be16b9853707e5a84df17f620cf20adac70e
Ian Zepp <ian.zepp@protonmail.com>
2023-09-07 09:19:21 -0400

    Merged migration files into a single `startup` migration

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-npm run compile
+
+# Compile everything
+if ! npm run compile; then
+    echo "Compilation failed!"
+    exit 1
+fi
+
+# Run the migration
+knex migrate:latest \
+    --client postgresql \
+    --connection $DATABASE_URL \
+    --migrations-directory=./dst/automigrate
 
 # Start node
 jest
--- src/classes/helpers.ts
+++ src/classes/helpers.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex';
 
 export async function migrationInsertAll(knex: Knex, table_name: string, record_data: _.Dictionary<any>[]): Promise<_.Dictionary<any>> {
     for(let record of record_data) {
-        console.warn('migrationInsert', record);
+        console.debug('insertAll: %j', record);
 
         let result = _.head(await knex(table_name).insert(record).returning('*'));
         let record_info = await knex(table_name + '_info').insert({ id: result.id, ns: record.ns });
