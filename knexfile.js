45da0894935bc6fa9b7524620e5975474f6c33e3
Ian Zepp <ian.zepp@protonmail.com>
2023-09-07 12:34:18 -0400

    Changed CI to use development env

--- knexfile.js
+++ knexfile.js
@@ -20,19 +20,5 @@ module.exports = {
             tableName: 'knex_migrations',
             directory: './dst/automigrate'
         }
-    },
-    
-    ci: {
-        client: 'postgresql',
-        connection: {
-            host:     process.env.POSTGRES_HOST,
-            database: process.env.POSTGRES_DB,
-            user:     process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-            directory: './dst/automigrate'
-        }
     }
 };
