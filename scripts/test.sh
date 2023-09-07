c58ec199f750ea56971e995b8ce5b9353e459c33
Ian Zepp <ian.zepp@protonmail.com>
2023-09-07 12:38:13 -0400

    Changing typescript to a -D install

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Compile everything
-if ! npm run compile:clean; then
-    echo "Compilation failed!"
-    exit 1
-fi
-
 # Show PG vars
 echo "Using NODE_ENV=$NODE_ENV"
 echo "Using POSTGRES_HOST=$POSTGRES_HOST"
 echo "Using POSTGRES_DB=$POSTGRES_DB"
 echo "Using POSTGRES_USER=$POSTGRES_USER"
+
+# Compile everything
+if ! npm run compile:clean; then
+    echo "Compilation failed!"
+    exit 1
+fi
 
 # Run the migration
 knex migrate:latest
