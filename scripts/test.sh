ff38db982031bedd3a84e7dee8e79a65f54f215f
Ian Zepp <ian.zepp@protonmail.com>
2023-09-07 10:51:36 -0400

    Added explicit knexfile path

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,12 +7,13 @@ if ! npm run compile; then
 fi
 
 # Show PG vars
+echo "Using NODE_ENV=$NODE_ENV"
 echo "Using POSTGRES_HOST=$POSTGRES_HOST"
 echo "Using POSTGRES_DB=$POSTGRES_DB"
 echo "Using POSTGRES_USER=$POSTGRES_USER"
 
 # Run the migration
-knex migrate:latest
+knex --knexfile ./knexfile.js migrate:latest
 
 # Start node
 jest
