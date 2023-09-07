77861913b993357af92abb6a053ada71f6207f6c
Aiden Zepp <aiden.zepp@protonmail.com>
2023-09-07 11:20:01 -0400

    Still testing...

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,10 +12,6 @@ echo "Using POSTGRES_HOST=$POSTGRES_HOST"
 echo "Using POSTGRES_DB=$POSTGRES_DB"
 echo "Using POSTGRES_USER=$POSTGRES_USER"
 
-# Wait for PostgreSQL to start
-echo "Waiting for PostgreSQL to start..."
-sleep 10
-
 # Run the migration
 knex --knexfile ./knexfile.js --client postgresql migrate:latest
 
