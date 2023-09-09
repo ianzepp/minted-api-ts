6ceb2d0d21ca4474959acdcfc0c4094ed9c38e5d
Ian Zepp <ian.zepp@protonmail.com>
2023-09-08 22:28:08 -0400

    Added `immutable` column test

---
+++ src/observers/record.test-immutable.ts
@@ -0,0 +1,59 @@
+import _ from 'lodash';
+
+// Classes
+import { Observer } from '../classes/observer';
+import { ObserverFlow } from '../classes/observer-flow';
+import { Column } from '../classes/column';
+
+// Layouts
+import { ObserverRing } from '../layouts/observer';
+
+
+export default class extends Observer {
+    toName(): string {
+        return 'record.test-immutable';
+    }
+    
+    onSchema(): string {
+        return 'record';
+    }
+
+    onRing(): ObserverRing {
+        return ObserverRing.Test;
+    }
+
+    onCreate(): boolean {
+        return true;
+    }
+
+    onUpdate(): boolean {
+        return true;
+    }
+
+    onUpsert(): boolean {
+        return true;
+    }
+
+    async run(flow: ObserverFlow): Promise<void> {
+        for(let column of Object.values(flow.schema.columns)) {
+            if (column.immutable === false) {
+                continue;
+            }
+
+            for(let record of flow.change) {
+                let data = record.get(column);
+                let prev = record.old(column);
+
+                if (prev === null) {
+                    continue;
+                }
+
+                if (prev === data) {
+                    continue;
+                }
+
+                flow.fail(300, `"${column.column_name}" is immutable`, record);
+            }
+        }
+   }
+}
