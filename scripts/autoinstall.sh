0d48827766a9ae1f6f85a2149ba79262d8e2a4c9
Ian Zepp <ian.zepp@protonmail.com>
2023-09-10 16:02:18 -0400

    Updated autoinstall script to NOT auto-drop DB

--- scripts/autoinstall.sh
+++ scripts/autoinstall.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 
-# Delete the database
-if ! dropdb -e $POSTGRES_DB; then
-    echo "Failed to drop database '$POSTGRES_DB'!"
-    exit 1
-else
-    echo "Dropped database '$POSTGRES_DB'"
-    echo
-fi
-
-# Recreate the DB
-if ! createdb -e $POSTGRES_DB; then
-    echo "Failed to recreate database '$POSTGRES_DB'!"
-    exit 1
-else
-    echo "Recreated database '$POSTGRES_DB'"
-    echo
-fi
-
 # Run the migration from scratch
 bun ./src/autoinstall.ts
