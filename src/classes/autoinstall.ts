d6bf2bb23af9f1cc64f0024254e7843ae7e2b3db
Ian Zepp <ian.zepp@protonmail.com>
2023-09-11 11:08:50 -0400

    Reworked autoinstall process

--- src/classes/autoinstall.ts
+++ src/classes/autoinstall.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { Knex } from 'knex';
 
 import { System } from './system';
 import { SystemAsRoot } from './system';
@@ -9,56 +10,78 @@ import knexInsertAll from '../helpers/knex-insert-all';
 
 export class AutoInstall {
     public readonly system = new SystemAsRoot();
+
+    get knex() {
+        return this.system.knex.db;
+    }
     
     async up() {
-        let knex = KnexDriver;
+        // Current database name
+        let database_name = this.knex.client.config.connection.database;
 
-        await knex.raw('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
+        // Add `pgcrypto` so we can create UUIDs in the DB
+        await this.knex.raw('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
 
-        await knex.raw('CREATE SCHEMA IF NOT EXISTS system_data;');
-        await knex.raw('GRANT USAGE, CREATE ON SCHEMA system_data TO PUBLIC;');
-    
-        await knex.raw('CREATE SCHEMA IF NOT EXISTS system_meta;');
-        await knex.raw('GRANT USAGE, CREATE ON SCHEMA system_meta TO PUBLIC;');
-    
-        await knex.raw('CREATE SCHEMA IF NOT EXISTS system_acls;');
-        await knex.raw('GRANT USAGE, CREATE ON SCHEMA system_acls TO PUBLIC;');    
+        // Create namespace
+        await this.knex.raw('CREATE SCHEMA IF NOT EXISTS "system";');
+        await this.knex.raw('GRANT USAGE, CREATE ON SCHEMA "system" TO PUBLIC;');
 
-        // Create the master namespace table.
-        await knex.schema.withSchema('system_data').createTable('master', (table) => {
+        await this.knex.raw('CREATE SCHEMA IF NOT EXISTS "system__meta";');
+        await this.knex.raw('GRANT USAGE, CREATE ON SCHEMA "system__meta" TO PUBLIC;');
+
+        // System table is always visible
+        await this.knex.raw(`ALTER DATABASE "${ database_name }" SET search_path TO system, public;`);
+
+        // Start the tx
+        await this.knex.raw('BEGIN TRANSACTION;');
+
+        // Save a variable for future use in this tx
+        await this.knex.raw(`
+            CREATE TEMPORARY TABLE temp_session_data(
+                user_id UUID, 
+                user_ns TEXT,
+                user_ts TIMESTAMP
+            );
+
+            INSERT INTO temp_session_data (user_id, user_ns, user_ts) 
+            VALUES ('${ System.RootId }', '${ System.RootNs }', CURRENT_TIMESTAMP);
+        `)
+
+        // Create the master client table.
+        await this.knex.schema.withSchema('system').createTable('client', (table) => {
+            table.uuid('id').primary().defaultTo(this.knex.fn.uuid());
             table.string('ns').primary().unique().notNullable();
             table.string('client_name').notNullable();
         });
 
-        // Create the master namespace data records.
-        await knex('system_data.master').insert([
-            { ns: 'system', client_name: 'System' },
-            { ns: 'test', client_name: 'Test' },
+        // Create the master client data records.
+        await this.knex('client').withSchema('system').insert([
+            { ns: 'system', client_name: 'Minted API System' },
+            { ns: 'test', client_name: 'Minted API Test Runner' },
         ]);
 
         //
         // Meta definitions
         //
 
-        // Create the core tables
-        await knexCreateTable(knex, 'schema');
-        await knexCreateTable(knex, 'column');
-        await knexCreateTable(knex, 'test');
-        await knexCreateTable(knex, 'user');
+        // Create table `client`
 
-        // Define core table structure
-        await knex.schema.table('system_data.schema', (table) => {
+        // Create table `schema`
+        await this.tableUp('system', 'schema', (table) => {
             table.string('schema_name').notNullable();
             table.string('schema_type').notNullable().defaultTo('database');
+            table.string('description');
 
             table.boolean('external').defaultTo(false);
             table.boolean('metadata').defaultTo(false);
         });
 
-        await knex.schema.table('system_data.column', (table) => {
+        // Create table `column`
+        await this.tableUp('system', 'column', table => {
             table.string('schema_name').notNullable();
             table.string('column_name').notNullable();
             table.string('column_type').notNullable().defaultTo('text');
+            table.string('description');
 
             table.boolean('audited').defaultTo(false);
             table.boolean('immutable').defaultTo(false);
@@ -66,36 +89,30 @@ export class AutoInstall {
             table.boolean('internal').defaultTo(false);
             table.boolean('required').defaultTo(false);
             table.boolean('unique').defaultTo(false);
+
             table.integer('minimum');
             table.integer('maximum');
+            table.integer('precision');
         });
 
-        await knex.schema.table('system_data.test', (table) => {
+        // Create table `client_user`
+        await this.tableUp('system', 'client_user', table => {
             table.string('name').notNullable();
-            table.boolean('data_boolean');
-            table.decimal('data_decimal');
-            table.integer('data_integer');
-            table.text('data_text');
-        });
-
-        await knex.schema.table('system_data.user', (table) => {
-            table.string('username').notNullable();
         });
 
         //
-        // Data insert
+        // Data inserts
         //
 
         // Add data for `schema`
-        await knexInsertAll(knex, 'schema', [
+        await this.insertAll('system', 'schema', [
             { ns: 'system', schema_name: 'schema', schema_type: 'database', metadata: true },
             { ns: 'system', schema_name: 'column', schema_type: 'database', metadata: true },
-            { ns: 'system', schema_name: 'test', schema_type: 'database', metadata: false },
-            { ns: 'system', schema_name: 'user', schema_type: 'database', metadata: false },
+            { ns: 'system', schema_name: 'client_user', schema_type: 'database', metadata: false },
         ]);
 
         // Add data for `column`
-        await knexInsertAll(knex, 'column', [
+        await this.insertAll('system', 'column', [
             // Columns for 'schema'
             { ns: 'system', schema_name: 'schema', column_name: 'schema_name' },
             { ns: 'system', schema_name: 'schema', column_name: 'metadata', column_type: 'boolean' },
@@ -114,6 +131,7 @@ export class AutoInstall {
 
             { ns: 'system', schema_name: 'column', column_name: 'minimum', column_type: 'integer' },
             { ns: 'system', schema_name: 'column', column_name: 'maximum', column_type: 'integer' },
+            { ns: 'system', schema_name: 'column', column_name: 'precision', column_type: 'integer' },
 
             // Columns for 'test'
             { ns: 'system', schema_name: 'test', column_name: 'name', required: true },
@@ -123,13 +141,102 @@ export class AutoInstall {
             { ns: 'system', schema_name: 'test', column_name: 'data_text', column_type: 'text' },
 
             // Columns for 'user'
-            { ns: 'system', schema_name: 'user', column_name: 'username', column_type: 'text' },
+            { ns: 'system', schema_name: 'client_user', column_name: 'name', column_type: 'text' },
         ]);
 
-        // Add data for `user`
-        await knex('system_data.user').insert([
-            { ns: 'test', username: 'test' },
+        // Add data for `client_user`
+        await this.insertAll('system', 'client_user', [
+            { ns: System.RootNs, id: System.RootId, name: 'root' },
+            { ns: System.TestNs, id: System.TestId, name: 'test' },
         ]);
 
+        // Commit the transaction
+        await this.knex.raw('COMMIT;');
+    }
+
+    //
+    // Helpers
+    //
+
+    async tableUp(namespace: string, schema_name: string, columnFn: (table: Knex.CreateTableBuilder) => void): Promise<void> {
+        console.warn('autoinstall.tableUp()', namespace, schema_name);
+
+        let schema_path = [namespace, schema_name].join('.');
+
+        // Data table
+        await this.knex.schema.withSchema(namespace).createTable(schema_name, (table) => {
+            table.uuid('id').primary().defaultTo(this.knex.fn.uuid());
+            table.string('ns').references('ns').inTable('system.client').onDelete('CASCADE');
+
+            // Apply extra columns
+            columnFn(table);
+        });
+    
+        // Meta table
+        await this.knex.schema.withSchema(namespace + '__meta').createTable(schema_name, (table) => {
+            table.uuid('id').primary().references('id').inTable(schema_path).onDelete('CASCADE');
+            table.string('ns'); // parent data has an index to the client ns
+    
+            table.timestamp('created_at').index();
+            table.uuid('created_by').index();
+
+            table.timestamp('updated_at').index();
+            table.uuid('updated_by').index();
+
+            table.timestamp('expired_at').index();
+            table.uuid('expired_by').index();
+
+            table.timestamp('deleted_at').index();
+            table.uuid('deleted_by').index();
+
+            table.specificType('acls_full', 'uuid ARRAY').index();
+            table.specificType('acls_edit', 'uuid ARRAY').index();
+            table.specificType('acls_read', 'uuid ARRAY').index();
+            table.specificType('acls_deny', 'uuid ARRAY').index();
+        });
+
+        //
+        // Auto-insert metadata table
+        //
+
+        await this.knex.raw(`
+            -- Define a trigger to auto-insert a metadata table
+            CREATE OR REPLACE FUNCTION ${namespace}__${schema_name}_insert_meta_function()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                temp_user_id UUID;
+                temp_user_ts TIMESTAMP;
+            BEGIN
+                -- Find the current session vars
+                SELECT COALESCE(user_id, '00000000-0000-0000-0000-000000000000'), COALESCE(user_ts, CURRENT_TIMESTAMP)
+                  INTO temp_user_id, temp_user_ts 
+                  FROM temp_session_data 
+                 LIMIT 1;
+        
+                -- Create the meta data
+                INSERT INTO "${namespace}__meta"."${schema_name}" (id, ns, created_at, created_by)
+                VALUES (NEW.id, NEW.ns, temp_user_ts, temp_user_id);
+                
+                -- Done
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+        
+            CREATE TRIGGER ${namespace}__${schema_name}_insert_meta_trigger 
+            AFTER INSERT ON "${namespace}"."${schema_name}"
+            FOR EACH ROW
+            EXECUTE PROCEDURE ${namespace}__${schema_name}_insert_meta_function();
+        `);
+    }
+
+    async insertAll(namespace: string, schema_name: string, record_rows: _.Dictionary<any>[]) {
+        console.warn('autoinstall.insertAll()', namespace, schema_name);
+
+        for(let record_data of record_rows) {
+            console.warn('+', JSON.stringify(record_data));
+
+            let result = _.head(await this.knex(schema_name).withSchema(namespace).insert(record_data).returning('*'));
+            // let result_meta   = await this.knex(schema_meta).withSchema(namespace).insert({ id: result.id, ns: namespace });
+        }
     }
 }
