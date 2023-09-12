3f66d80296fe6804905e9418a3b522918a4babc9
Ian Zepp <ian.zepp@protonmail.com>
2023-09-12 00:13:38 -0400

    Converted server to a Bun HTTP server

--- src/start.ts
+++ src/start.ts
@@ -2,4 +2,11 @@
 import { HttpServer } from '@classes/http-server';
 
 // Create the http server
-new HttpServer().listen(8080);
+Bun.serve({
+    port: process.env.PORT || 8080,
+    
+    fetch(req: Request) {
+        return new HttpServer().route(req);
+    },
+});
+
