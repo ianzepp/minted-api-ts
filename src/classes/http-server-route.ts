f2457809a74fa1edd983384bfd2a32f1862fe079
Ian Zepp <ian.zepp@protonmail.com>
2023-09-13 23:07:10 -0400

    Deleted unused file

--- src/classes/http-server-route.ts
+++
@@ -1,9 +0,0 @@
-import { HttpRouter } from '@classes/http-router';
-
-export interface HttpServerRoute {
-    verb: string;
-    path: string;
-    path_regexp: RegExp,
-    router_type: typeof HttpRouter;
-}
-
