e461532ee2e8ce2f7f567a113d2b0b8f629a8954
Ian Zepp <ian.zepp@protonmail.com>
2023-09-25 14:30:48 -0400

    Updated `clean` script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,18 +5,33 @@ if ! bun tsc; then
     exit 1;
 fi
 
-# Drop the DB
-echo "Dropping and recreating the '$POSTGRES_DB' database.."
-echo ""
+# Postgres config
+if [ "$KNEX_CLIENT" = "postgres" ]; then
+    echo "Using knex config:"
+    echo "- client = $KNEX_CLIENT"
+    echo "- database = $KNEX_DATABASE"
+    
+    if [ -n "$KNEX_DATABASE" ] && ! dropdb "$KNEX_DATABASE"; then
+        echo "Knex(postgres): Failed to drop database '$KNEX_DATABASE'";
+        exit 1
+    fi
 
-if ! dropdb "$POSTGRES_DB"; then
-    echo "Failed to drop database";
-    exit 1
+    if [ -n "$KNEX_DATABASE" ] && ! createdb "$KNEX_DATABASE"; then
+        echo "Knex(postgres): Failed to recreate database '$KNEX_DATABASE'";
+        exit 1
+    fi
 fi
 
-if ! createdb "$POSTGRES_DB"; then
-    echo "Failed to recreate database";
-    exit 1
+# Sqlite config
+if [ "$KNEX_CLIENT" = "sqlite3" ]; then
+    echo "Using knex config:"
+    echo "- client = $KNEX_CLIENT"
+    echo "- filename = $KNEX_FILENAME"
+
+    if [ -n "$KNEX_FILENAME" ] && ! rm -f "$KNEX_FILENAME"; then
+        echo "Knex(sqlite3): Failed to remove database file: $KNEX_FILENAME";
+        exit 1
+    fi
 fi
 
 # Run the migration from scratch
