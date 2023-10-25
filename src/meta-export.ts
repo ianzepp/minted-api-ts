9badc4ea4cd818be49d3c3d62b07de9bbe650d7a
Ian Zepp <ian.zepp@protonmail.com>
2023-10-25 08:05:31 -0400

    Export process is implemented and cleaned up

--- src/meta-export.ts
+++ src/meta-export.ts
@@ -4,67 +4,78 @@ import fs from 'fs-extra';
 // Classes
 import { Kernel } from "./system/kernels/kernel";
 import { Object, ObjectType } from './system/classes/object';
+import { Column } from './system/classes/column';
+import { Record } from './system/classes/record';
 
 export class MetaExport {
     readonly kernel = new Kernel();
     readonly base = './tmp/exports';
 
+    path(object_name, record_name) {
+        return `./${ this.base }/${ object_name }/${ record_name }.json`;
+    }
+
     async export() {
         await this.kernel.startup();
 
-        // Console.warn
-        console.warn('Exporting to:', this.base);
-
-        // Clean out the target
-        await fs.emptyDir(this.base);
-
-        // Process
-        for(let object of this.kernel.meta.objects) {
-            let object_path = `./${ this.base }/system::object/${ object.system_name }.json`;
-            let object_json = JSON.stringify(object.toJSON(), null, 4);
-
-            console.info('object:', object.system_name);
-
-            // Output the object data
-            await fs.createFile(object_path);
-            await fs.writeFile(object_path, object_json);
-
-            // Process columns
-            for(let column of _.values(object.columns)) {
-                console.info('- column:', column.system_name);
-
-                let column_path = `./${ this.base }/system::column/${ object.system_name }.${ column.system_name }.json`;
-                let column_json = JSON.stringify(column.toJSON(), null, 4);
-
-                // Output the column data
-                await fs.createFile(column_path);
-                await fs.writeFile(column_path, column_json);
-            }
-
-            // Don't export records for the two core types
-            if (object.system_name === ObjectType.Object) {
-                continue;
-            }
-
-            if (object.system_name === ObjectType.Column) {
-                continue;
-            }
-
-            // Export records
-            for(let record of await this.kernel.data.searchAny(object)) {
-                console.info('- record:', record.data.id);
-
-                let record_path = `./${ this.base }/${ object.system_name }/${ record.data.id }.json`;
-                let record_json = JSON.stringify(_.omit(record.toJSON(), ['meta', 'acls']), null, 4);
-
-                // Output the record data
-                await fs.createFile(record_path);
-                await fs.writeFile(record_path, record_json);
-            }
-        }
+        await this.cleanupDirectory();
+        await this.exportObjectList();
+        
+        await this.kernel.cleanup();
 
         // Done
         process.exit();
+    }
+
+    async cleanupDirectory() {
+        return fs.emptyDir(this.base);
+    }
+
+    async exportObjectList() {
+        for(let object of this.kernel.meta.objects) {
+            await this.exportObject(object);
+        }
+    }
+
+    async exportObject(object: Object) {
+        await this.exportJson('system::object', object.system_name, object.toJSON());
+        await this.exportColumnList(object);
+        await this.exportRecordList(object);
+    }
+
+    async exportColumnList(object: Object) {
+        for(let column of _.values(object.columns)) {
+            await this.exportColumn(object, column);
+        }        
+    }
+
+    async exportColumn(object: Object, column: Column) {
+        await this.exportJson('system::column', `${ object.system_name }.${ column.system_name }`, column.toJSON());
+    }
+
+    async exportRecordList(object: Object) {
+        // Don't export records for the two core types
+        if (object.system_name === ObjectType.Object) {
+            return;
+        }
+
+        if (object.system_name === ObjectType.Column) {
+            return;
+        }
+        
+        for(let record of await this.kernel.data.searchAny(object)) {
+            await this.exportRecord(object, record);
+        }
+    }
+
+    async exportRecord(object: Object, record: Record) {
+        await this.exportJson(object.system_name, record.data.id, _.omit(record.toJSON(), ['meta', 'acls']));
+    }
+
+    async exportJson(object_name: string, record_name: string, json: object) {
+        let record_path = this.path(object_name, record_name);
+        let record_json = JSON.stringify(json, null, 4);
+        await fs.createFile(record_path).then(() => fs.writeFile(record_path, record_json));
     }
 }
 
